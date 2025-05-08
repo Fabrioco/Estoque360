f4843ee022aee8096266ac7c38b77ae6caf720e9
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-08 17:14:16 -0300

    :sparkles: feat: Adicionei o configModule pra usar o .env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,16 +2,18 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "projetoestoque",
-      database: "estoque",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + "/**/*.entity{.ts,.js}"],
       synchronize: true,
     }),
