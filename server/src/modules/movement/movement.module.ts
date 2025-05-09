26b532175ed36624daef69852bc1de30c02f23da
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-09 07:33:25 -0300

    :sparkles: feat: Adicionei o entity do produto

--- server/src/modules/movement/movement.module.ts
+++ server/src/modules/movement/movement.module.ts
@@ -3,10 +3,11 @@ import { MovementService } from './movement.service';
 import { MovementController } from './movement.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movement } from './entities/movement.entity';
+import { Product } from '../product/entities/product.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Movement])
+    TypeOrmModule.forFeature([Movement, Product])
   ],
   controllers: [MovementController],
   providers: [MovementService],
