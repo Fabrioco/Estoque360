9b7eeb86bfed4d3ba6f2f3b624bd6fd729a52256
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-09 20:05:06 -0300

    :boom: fix: Corrigi o layout pra não mostrar a barra de navegação

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
+import "@/src/styles/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
