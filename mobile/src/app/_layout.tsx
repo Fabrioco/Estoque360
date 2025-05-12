d343737bfb40037c40091f0e93759b7d3dbbe3ae
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-12 11:55:45 -0300

    :sparkles: feat: Adicionei um layout de header e footer

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,6 +1,15 @@
 import { Slot } from "expo-router";
 import "@/src/styles/global.css";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <View className="flex-1">
+      <Text>Meu App</Text>
+
+      <Slot screenOptions={{ headerShown: false }} />
+
+      <Text>@ 2025 - Todos os direitos reservados</Text>
+    </View>
+  );
 }
