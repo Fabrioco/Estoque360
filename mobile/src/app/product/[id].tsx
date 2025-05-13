e2910acb69af1066f5afc4b57df58c303d5c3950
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-12 22:47:28 -0300

    :lipstick: feat: Fiz a estilização dos campos e botão de voltar pra página anterior

--- mobile/src/app/product/[id].tsx
+++ mobile/src/app/product/[id].tsx
@@ -9,6 +9,7 @@ import {
   ActivityIndicator,
   Alert,
   SafeAreaView,
+  ScrollView,
   Text,
   TextInput,
   TouchableOpacity,
@@ -115,68 +116,84 @@ export default function ScreenProduct() {
   }
 
   return (
-    <SafeAreaView>
-      <View>
-        <ProductField
-          label="Nome do produto"
-          value={product.name}
-          onChange={(text) => handleChange("name", text)}
-          editable={editable}
-          keyboardType="default"
-        />
-
-        <ProductField
-          label="Descrição do produto"
-          value={product.description || ""}
-          onChange={(text) => handleChange("description", text)}
-          editable={editable}
-          keyboardType="default"
-        />
-
-        <ProductField
-          label="Quantidade atual"
-          value={product.currentQuantity}
-          onChange={(number) => handleChange("currentQuantity", number)}
-          editable={editable}
-          keyboardType="numeric"
-        />
-
-        <ProductField
-          label="Quantidade minima"
-          value={product.minQuantity}
-          onChange={(number) => handleChange("minQuantity", number)}
-          editable={editable}
-          keyboardType="numeric"
-        />
-
-        <ProductField
-          label="Preço de compra"
-          value={product.purchasePrice}
-          onChange={(number) => handleChange("purchasePrice", number)}
-          editable={editable}
-          keyboardType="numeric"
-        />
-
-        <ProductField
-          label="Preço de venda"
-          value={product.salePrice}
-          onChange={(number) => handleChange("salePrice", number)}
-          editable={editable}
-          keyboardType="numeric"
-        />
-
-        <View>
+    <SafeAreaView className="flex-1 items-center">
+      <ScrollView className="flex-1 w-full">
+        <View className="flex-row justify-end w-11/12 items-center">
           <TouchableOpacity
-            onPress={editable ? handleEditProduct : () => setEditable(true)}
+            onPress={() => router.back()}
+            className="bg-black rounded-full px-4 py-2 w-fit"
           >
-            <Text>{editable ? "Salvar" : "Editar"}</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={handleDeleteProduct}>
-            <Text>Excluir</Text>
+            <Text className="text-xl font-bold text-white">Voltar</Text>
           </TouchableOpacity>
         </View>
-      </View>
+        <View className="flex-col gap-4 w-11/12 mx-auto mt-4">
+          <ProductField
+            label="Nome do produto"
+            value={product.name}
+            onChange={(text) => handleChange("name", text)}
+            editable={editable}
+            keyboardType="default"
+          />
+
+          <ProductField
+            label="Descrição do produto"
+            value={product.description || ""}
+            onChange={(text) => handleChange("description", text)}
+            editable={editable}
+            keyboardType="default"
+          />
+
+          <ProductField
+            label="Quantidade atual"
+            value={product.currentQuantity}
+            onChange={(number) => handleChange("currentQuantity", number)}
+            editable={editable}
+            keyboardType="numeric"
+          />
+
+          <ProductField
+            label="Quantidade minima"
+            value={product.minQuantity}
+            onChange={(number) => handleChange("minQuantity", number)}
+            editable={editable}
+            keyboardType="numeric"
+          />
+
+          <ProductField
+            label="Preço de compra"
+            value={product.purchasePrice}
+            onChange={(number) => handleChange("purchasePrice", number)}
+            editable={editable}
+            keyboardType="numeric"
+          />
+
+          <ProductField
+            label="Preço de venda"
+            value={product.salePrice}
+            onChange={(number) => handleChange("salePrice", number)}
+            editable={editable}
+            keyboardType="numeric"
+          />
+
+          <View className="flex-row gap-4 w-11/12 mx-auto mt-4">
+            <TouchableOpacity
+              className="bg-white rounded-full px-4 py-2 "
+              onPress={editable ? handleEditProduct : () => setEditable(true)}
+            >
+              <Text className="text-xl font-bold text-black">
+                {editable ? "Salvar" : "Editar"}
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              className="bg-white rounded-full px-4 py-2 "
+              onPress={handleDeleteProduct}
+            >
+              <Text className="text-xl font-bold text-black">Excluir</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
