88ec94bd43833fe9c12633acbe71ff8937172217
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-13 13:16:24 -0300

    :lipstick: feat: Criei a estilização da tela de movimentações

--- mobile/src/app/(tabs)/movements/index.tsx
+++ mobile/src/app/(tabs)/movements/index.tsx
@@ -75,72 +75,90 @@ export default function Movements() {
   };
 
   return (
-    <SafeAreaView>
-      <View>
-        <Text>Movimentações</Text>
-      </View>
-
-      <View>
-        <Text>Lista de movimentações</Text>
-        <TouchableOpacity onPress={() => setIsModalOpen(true)}>
-          <Text>
-            Novo <PlusCircle size={20} />
+    <SafeAreaView className="flex-1 items-center bg-slate-200">
+      <View className="flex-col gap-2 w-11/12 mx-auto">
+        <View className="flex-row items-center justify-start">
+          <Text className="font-bold text-2xl uppercase w-full">
+            Movimentações
           </Text>
-        </TouchableOpacity>
-      </View>
+        </View>
 
-      {typeof movements === "string" ? (
-        <Text>{movements}</Text>
-      ) : (
-        <ScrollView>
-          {movements.map((movement) => (
-            <View key={movement.id}>
-              <Text>Produto: {movement.name}</Text>
+        <View className="flex-row items-center justify-between">
+          <TouchableOpacity
+            onPress={() => setIsModalOpen(true)}
+            className="flex-row items-center gap-2 bg-white rounded-full px-4 py-2 shadow-md"
+          >
+            <Text className="text-xl font-bold text-black">
+              Novo <PlusCircle size={20} weight="bold" />
+            </Text>
+          </TouchableOpacity>
+        </View>
 
-              <View>
-                <Text>Tipo</Text>
-                <TextInput
-                  value={movement.type}
-                  keyboardType="default"
-                  editable={false}
-                />
-              </View>
-
-              <View>
-                <Text>Quantidade</Text>
-                <Text>{movement.quantity}</Text>
-              </View>
-
-              <View>
-                <Text>Data</Text>
-                <Text>{movement.date?.split("T")[0] || ""}</Text>
-              </View>
-
-              <View>
-                <Text>Hora</Text>
-                <Text>
-                  {movement.date?.split("T")[1].split(".")[0].slice(0, 5) || ""}
-                </Text>
-              </View>
-
-              <View>
-                <Text>Motivo</Text>
-                <Text>{movement.reason ? movement.reason : "(Opcional)"}</Text>
-              </View>
-              <TouchableOpacity
-                onPress={() => router.push(`/movement/${movement.id}`)}
+        {typeof movements === "string" ? (
+          <Text className="text-2xl">{movements}</Text>
+        ) : (
+          <ScrollView className="w-full h-full">
+            {movements.map((movement) => (
+              <View
+                key={movement.id}
+                className="flex-row gap-2 items-center justify-between bg-white rounded-full px-4 py-2 shadow-md "
               >
-                <Text>Ver mais</Text>
-              </TouchableOpacity>
-            </View>
-          ))}
-        </ScrollView>
-      )}
+                <View className="flex-col w-2/12">
+                  <Text className="font-bold text-lg">Produto</Text>
+                  <Text>{movement.name}</Text>
+                </View>
 
-      <ModalMovements
-        isModalVisible={isModalOpen}
-        setModalVisible={setIsModalOpen}
-      />
+                <View className="flex-col">
+                  <View className="flex-row gap-1">
+                    <Text className="font-bold">Tipo:</Text>
+                    <Text>
+                      {movement.type === "ENTRADA" ? "Entrada" : "Saida"}
+                    </Text>
+                  </View>
+
+                  <View className="flex-row gap-1">
+                    <Text className="font-bold">Quantidade:</Text>
+                    <Text>{movement.quantity}</Text>
+                  </View>
+                </View>
+
+                <View className="flex-col">
+                  <View className="flex-row gap-1">
+                    <Text className="font-bold">Data:</Text>
+                    <Text>
+                      {movement?.date
+                        ?.split("T")[0]
+                        .split("-")
+                        .reverse()
+                        .join("/") || ""}
+                    </Text>
+                  </View>
+
+                  <View className="flex-row gap-1">
+                    <Text className="font-bold">Hora:</Text>
+                    <Text>
+                      {movement.date?.split("T")[1].split(".")[0].slice(0, 5) ||
+                        ""}
+                    </Text>
+                  </View>
+                </View>
+
+                <TouchableOpacity
+                  onPress={() => router.push(`/movement/${movement.id}`)}
+                  className="flex-row items-center gap-2 bg-black rounded-full px-4 py-2 shadow-md"
+                >
+                  <Text className="text-xl font-bold text-white">Ver mais</Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </ScrollView>
+        )}
+
+        <ModalMovements
+          isModalVisible={isModalOpen}
+          setModalVisible={setIsModalOpen}
+        />
+      </View>
     </SafeAreaView>
   );
 }
