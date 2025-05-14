aab3029aae8a0a3a4bce082d8461c973562c8ef4
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-14 15:22:41 -0300

    :boom: fix: Correção do titiulo do descer quando abrir o modal

--- mobile/src/components/header.tsx
+++ mobile/src/components/header.tsx
@@ -4,8 +4,8 @@ import { BoxArrowUp } from "phosphor-react-native";
 
 export function Header() {
   return (
-    <View className="w-11/12 mx-auto flex-row items-center justify-between bg-white rounded-b-xl px-4 py-2 mb-4">
-      <View className="flex-row items-center">
+    <View className="w-11/12 mx-auto flex-row  justify-between bg-white rounded-b-xl px-4 py-2 mb-4 relative">
+      <View className="flex-row items-center max-h-10">
         <BoxArrowUp size={32} />
         <Text className="font-bold text-2xl">Estoque360</Text>
       </View>
