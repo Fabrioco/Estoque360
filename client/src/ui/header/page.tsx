5244077dcca518e725742b69d0fcfff117d95876
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-15 17:29:49 -0300

    :lipstick: fix: Corrigi o titulo que descia quando abria as notificações

--- client/src/ui/header/page.tsx
+++ client/src/ui/header/page.tsx
@@ -5,8 +5,8 @@ import { Notification } from "./notification";
 export function Header() {
   return (
     <div className="flex flex-col items-center justify-between bg-white rounded-b-xl px-4 py-2 mb-4 w-11/12 mx-auto gap-2">
-      <div className="flex flex-row items-center justify-between w-full">
-        <h1 className="flex flex-row items-center justify-center text-2xl font-bold">
+      <div className="flex flex-row  justify-between w-full gap-2">
+        <h1 className="flex flex-row items-center justify-center text-2xl font-bold max-h-10">
           <BoxArrowUp size={32} weight="bold" />
           Estoque360
         </h1>
