27b6ce5a3699c4a2d90c585bb50523492ddf70a1
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-15 11:48:35 -0300

    :sparkles: feat: Adicionei o header pra uso global

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/ui/header/page";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased bg-slate-200 min- w-screen min-h-screen`}
       >
+        <Header />
         {children}
       </body>
     </html>
