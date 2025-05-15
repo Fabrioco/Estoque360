a1904f3a0cef93b83e80cfba0f7b41ede27ecdb9
Fabrioco <fabriciooliveiralopes50@gmail.com>
2025-05-14 22:23:10 -0300

    :sparkles: feat: Criei a tipagem do produto

---
+++ client/src/types/productType.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  id?: number;
+  name: string;
+  currentQuantity: number;
+  minQuantity: number;
+  purchasePrice: number;
+  salePrice: number;
+  description?: string;
+};
